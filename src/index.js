c763abe0a42e910dbf5b3702e80029ad8cf64354
Subramaniyam-cpu <2022cfse004@wilp.bits-pilani.ac.in>
2022-10-29 13:56:17 -0800

    d
    Neww

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+/*const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way of programming "
+h1.className = "header"
+document.getElementById('root').append(h1)*/
+function tempname(){
+    return ( <nav>
+        <h1>Bob's shop</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>);
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+root.render( tempname());
+
+;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
